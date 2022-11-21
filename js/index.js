e1d05e7fc1445fc1835fed779a7de3d7374322e7
TTTin239 <trantrungtin2309@gmail.com>
2022-11-21 13:16:51 +0700

    virtual assistant update

--- js/index.js
+++ js/index.js
@@ -28,3 +28,91 @@ searchInput.addEventListener('change', (e)=>{
             windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE;
         });
 })
+
+// Trợ lý ảo
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+const synth = window.speechSynthesis;
+recognition.lang = 'vi-VI';
+recognition.continous = false;
+
+const microphone = document.querySelector('.microphone');
+
+const speak = (text) => {
+    if (synth.speaking) {
+        console.error('Busy. Speakig...');
+        return;
+    }
+
+    const utter = new SpeechSynthesisUtterance(text);
+
+    utter.onend = () => {
+        console.log('SpeechSynthesisUtterance.onend');
+    }
+    utter.onerror = (err) => {
+        console.error('SpeechSynthesisUtterance.onerror', err);
+    }
+
+    synth.speak(utter);
+};
+
+const handleVoice = (text) => {
+    console.log(text);
+
+    // "Thời tiết tại Đà Nẵng" => ["Thời tiết tại", "Đà Nẵng"]
+    const handledText = text.toLowerCase();
+    if(handledText.includes('thời tiết tại')){
+        const location = handledText.split('tại')[1].trim();
+        console.log('location', location);
+        searchInput.value = location;
+        const changeEvent = new Event('change');
+        searchInput.dispatchEvent(changeEvent);
+        return;
+    }
+
+    const container = document.querySelector('.container');
+    // thay đổi màu nền [Black, white, orange, ...]
+    if(handledText.includes('thay đổi màu nền')){
+        const color = handledText.split('màu nền')[1].trim();
+        container.style.background = color;
+        return;
+    }
+
+    // reset lại màu nền mặc định
+    if (handledText.includes('màu nền mặc định')){
+        container.style.background = '';
+        return;
+    }
+
+    // xem giờ
+    if (handledText.includes('mấy giờ')){
+        const textToSpeech = `${moment().hours()} hours ${moment().minutes()} minutes`
+        speak(textToSpeech);
+        return;
+    }
+
+    speak('Try again');
+}
+
+microphone.addEventListener('click', (e) =>{
+    e.preventDefault();
+    recognition.start();
+    microphone.classList.add('recording');
+});
+
+recognition.onspeechend = () =>{
+    recognition.stop();
+    microphone.classList.remove('recording');
+}
+
+recognition.onerror = (err) => {
+    console.error(err);
+    microphone.classList.remove('recording');
+}
+
+recognition.onresult = (e) => {
+    console.log('onresult', e);
+    const text = e.results[0][0].transcript;
+    handleVoice(text);
+}
